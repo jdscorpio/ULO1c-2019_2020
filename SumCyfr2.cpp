0b0e90a4272ceae0389a2aa48709bc84b51f5e96
Jaroslaw Drzezdzon <jd@vlo.gda.pl>
2020-01-07 14:56:36 +0100

    Add files via upload

---
+++ SumCyfr2.cpp
@@ -0,0 +1,24 @@
+/// Suma cyfr liczby ca³kowitej
+/// Autor: Jaros³aw Drze¿d¿on
+/// Data: 2020-01-07
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number, r;
+    int sum = 0;
+    cin >> number;
+
+    while(number>0)
+    {
+        r = number % 10;
+        sum += r;
+        number /= 10;
+    }
+    cout << sum;
+
+    return 0;
+}
